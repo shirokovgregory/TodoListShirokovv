003186916ffd9866c9bf3cada2fd4094e3858a89
shirokovgregory <lleeq77@mail.ru>
2024-01-08 21:00:06 +0300

    Laboratornaya 4 chetyre

---
+++ TodoListShirokovv/Repos/IRepo.cs
@@ -0,0 +1,10 @@
+﻿using TodoListShirokovv.Presenter;
+
+namespace TodoListShirokovv.Repos;
+
+public interface IRepo
+{
+    Task<List<TodoTask>> SearchTask(string tag);
+    Task? AddTask(TodoTaskDto task);
+    Task<Dictionary<string, List<TodoTask>>> ShowAllTasks();
+}
---
+++ TodoListWebApi/Controllers/Conroller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListShirokovv.Presenter;
+using TodoListShirokovv.Repos;
+
+namespace TodoListWebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Conroller
+{
+    private readonly IRepo _repository;
+    
+    public Conroller(IRepo repository)
+    {
+        _repository = repository;
+    }
+    
+    [HttpGet] [Route("/search-task-by-tag")]
+    public async Task<List<TodoTask>> Search([FromQuery] string tag)
+    {
+        return await _repository.SearchTask(tag);
+
+    }
+
+    [HttpPost]
+    [Route("/add-new-task")]
+    public Task AddTodoTask([FromBody] TodoTaskDto task)
+    {
+        return _repository.AddTask(task);
+    }
+
+    [HttpGet]
+    [Route("/show-all-tasks")]
+    public async Task<ActionResult<Dictionary<string, List<TodoTask>>>> LastTasks()
+    {
+        return await _repository.ShowAllTasks();
+    }
+}
